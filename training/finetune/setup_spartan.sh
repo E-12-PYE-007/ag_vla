b5a17d92139894aea529768292de239f7a09eaa3
lyamamoto <164627499+lyamatomato@users.noreply.github.com>
2026-08-15 14:56:16 +1000

    Minor fix

--- training/finetune/setup_spartan.sh
+++ training/finetune/setup_spartan.sh
@@ -55,7 +55,7 @@ pip install \
 ok "PyTorch 2.2.0 (cu121) installed"
 
 info "Installing AsyncVLA base requirements..."
-grep -v "OmniVLA" "${ASYNCVLA_DIR}/requirements.txt" | pip install -r /dev/stdin --quiet
+grep -v "OmniVLA\|vint_train" "${ASYNCVLA_DIR}/requirements.txt" | pip install -r /dev/stdin --quiet
 ok "AsyncVLA requirements installed"
 
 # Install editable packages
