a5713716375f26826647d2e859142351f36a0857
lyamamoto <164627499+lyamatomato@users.noreply.github.com>
2026-08-15 13:42:15 +1000

    Set project dir correctly

--- training/finetune/setup_spartan.sh
+++ training/finetune/setup_spartan.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 # Set environment variables
 PROJECT_ID=capstone
-PROJECT_DIR="/capstone/E12PYE007"
+PROJECT_DIR="/data/gpfs/projects/capstone/E12PYE007"
 VENV_DIR="${PROJECT_DIR}/venvs/training/finetune"
 
 PYTHON_MODULE="GCCcore/11.3.0 Python/3.10.4"
